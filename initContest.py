aba63ca55c26e73c963c7dcf7a8ab5be7a7d0808
danghoangnam <88759047+danghoangnam@users.noreply.github.com>
2024-10-24 13:11:58 +0700

    Thêm chức năng tạo contest

---
+++ initContest.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+baiLam = "baiLam"
+deThi = "deThi"
+test = "test"
+
+def ifExistContest() -> bool:
+    return os.path.exists(baiLam) or os.path.exists(deThi) or os.path.exists(test)
+
+def init():
+
+    if os.path.exists(baiLam):
+        shutil.rmtree(baiLam)
+        pass
+    os.mkdir(baiLam)
+
+    if os.path.exists(deThi):
+        shutil.rmtree(deThi)
+        pass
+    os.mkdir(deThi)
+
+    if os.path.exists(test):
+        shutil.rmtree(test)
+        pass
+    os.mkdir(test)
